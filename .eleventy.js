3c0e4148bd3c0523e13172c8fb4a57616dc21454
Mike Aparicio <peruvianidol@gmail.com>
2021-02-06 22:56:43 -0600

    Add the fonts you dummy

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,7 @@
 module.exports = function(eleventyConfig) {
   eleventyConfig.addWatchTarget("./_src/sass/");
   eleventyConfig.addPassthroughCopy("./_src/images/");
+  eleventyConfig.addPassthroughCopy("./_src/fonts/");
 
   return {
     dir: {
